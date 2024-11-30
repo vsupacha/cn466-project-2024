d2c0e105b3136079a1588146aee95abd65ddeb48
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-11-30 17:13:07 +0700

    Backend users

--- docker/room_api/routes/user.py
+++ docker/room_api/routes/user.py
@@ -6,7 +6,6 @@ user_blueprint = Blueprint('user', __name__)
     
 @user_blueprint.route("/list", methods=["GET"])
 def user_list():
-    """Fetch all users and render the user list page."""
     users_json = mongo_user_command_list()
     try:
         users = json.loads(users_json)
@@ -18,12 +17,11 @@ def user_list():
 
 @user_blueprint.route("/history/<user_id>", methods=["GET"])
 def user_history(user_id: str):
-    """Fetch user history and render the user history page."""
     user_json = mongo_get_user_history(user_id)
     try:
-        user = json.loads(user_json)
-        if user:
-            return render_template('history.html', user=user)
+        users = json.loads(user_json)
+        if users:
+            return render_template('history.html', users=users)
         return jsonify({"error": "User not found"}), 404
     except json.JSONDecodeError:
         return jsonify({"error": "Error decoding user data"}), 500
